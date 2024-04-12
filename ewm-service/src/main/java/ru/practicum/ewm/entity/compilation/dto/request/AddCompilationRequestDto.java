5637ddf1fe06128d75f846b2a4b8dd61e2273803
Setsuma <pilipas.s.a@yandex.ru>
2024-04-12 11:17:56 +0300

    dev

--- ewm-service/src/main/java/ru/practicum/ewm/entity/compilation/dto/request/AddCompilationRequestDto.java
+++ ewm-service/src/main/java/ru/practicum/ewm/entity/compilation/dto/request/AddCompilationRequestDto.java
@@ -5,7 +5,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
